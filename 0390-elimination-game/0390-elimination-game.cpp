b71c547e829873223d952b5c18e5c035751c8460
Rajnish Prasad Kalwar <98279952+Rajnishkalwar@users.noreply.github.com>
2023-07-31 23:45:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.16%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-       if(n==1)
+       if(n==1)                 // if there is only one number present then return the number
                return n;
             
         return 2*(1+n/2-lastRemaining(n/2));
     }
 };
+ //                 1 2 3 4 5 6 7 8......
+ // from L T R        2*(1,2,3,4,.....n/2)
+ //                    2*f(n/2)
+ // from R t L         2*(n/2 ......3,2,1)
+ //                         so on a single conclusion
+ //                         1+n/2-1=n/2
+ //                         1+n/2-n/2=1
+         
